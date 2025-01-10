bf82d74106e536dc4a6e49e41b9506cb12a1e951
Quang Huy <huytrinh1407@gmail.com>
2025-01-10 10:36:00 +0700

    jjj

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 Rails.application.routes.draw do
-  resources :products
+  resource :unsubscribe, only: [ :show ]
+
+  resource :session
+  resources :passwords, param: :token
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
 
   get "/products", to: "products#index"
 
   get "/products/new", to: "products#new"
   post "/products", to: "products#create"
-  
+
   get "/products/:id", to: "products#show"
-  
+
   get "/products/:id/edit", to: "products#edit"
   patch "/products/:id", to: "products#update"
   put "/products/:id", to: "products#update"
-  
-  delete "/products/:id", to: "products#destroy"
-  
-  root "products#index"
 
-  
+  delete "/products/:id", to: "products#destroy"
+
+  root "products#index"
 end
